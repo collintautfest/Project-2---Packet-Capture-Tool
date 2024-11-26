980fa90b15c7ae1a4a678535f3e5703705afc79a
collintautfest <ctautfest1@gmail.com>
2024-11-26 14:45:30 -0500

    filtered progress

--- Code/filter_packets.py
+++ Code/filter_packets.py
@@ -1,3 +1,11 @@
-def filter() :
-	print('called filter function in filter_packets.py')
-	
+#!/bin/python3
+def filter():
+    with open('/home/student/project2/Project-2---Packet-Capture-Tool/Captures/Captures/Node1.txt', 'r') as file:
+        for line in file:
+            # search for lines containing the icmp echo request or reply marker
+            # send to filtered file
+
+
+    # Output
+
+filter()	
