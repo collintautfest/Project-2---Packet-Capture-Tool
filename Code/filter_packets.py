5ae255c53dfbf2f93b8d1716b89878da4ee128d8
collintautfest <ctautfest1@gmail.com>
2024-11-27 21:35:34 -0500

    minor update for output file and input file

--- Code/filter_packets.py
+++ Code/filter_packets.py
@@ -1,12 +1,23 @@
 #!/bin/python3
 def filter(file_name):
-    filename = file_name
-    with open(filename, 'r') as file:
-        for line in file:
-            # search for lines containing the icmp echo request or reply marker
-            # send to filtered file
+    try:
+        filename = file_name
+        
+        base, ext = filename.rsplit('.', 1)
+        output_file = f"{base}_filtered.{ext}"
+
+        with open(filename, 'r') as file, open(output_file, 'w') as outfile:
+            for line in file:
+                # search for lines containing the icmp echo request or reply marker
+                split_line = line.strip().split()
+
+                if split_line[] == "ICMP"
+                # send to filtered file
 
 
-    # Output
+        # Output
+    except:
+        print(f"Error: The file '{file_name}' does not exist.")
+
 
 filter()	
