7b6280a217001bbe31acf513d6e143d50cda977d
collintautfest <ctautfest1@gmail.com>
2024-11-29 16:17:32 -0500

    testing, code partially works however I am having a small issue with the try except causing it to write blank files

--- Code/filter_packets.py
+++ Code/filter_packets.py
@@ -7,17 +7,35 @@ def filter(file_name):
         output_file = f"{base}_filtered.{ext}"
 
         with open(filename, 'r') as file, open(output_file, 'w') as outfile:
+            write_packet = False # determine if start writing lines
+            Buffer = [] # buffer to store packets
             for line in file:
-                # search for lines containing the icmp echo request or reply marker
-                split_line = line.strip().split()
+                if "No." in line and "ICMP" in line:
+                    # start writing
+                    if buffer:
+                        outfile.write("".join(buffer)) # write previous
+                        buffer = [] # reset
 
-                if split_line[] == "ICMP"
-                # send to filtered file
+                    write_packet = True
+                    buffer.append(line)
+                elif "No." in line:
+                    # end previous packet if not icmp
+                    if buffer and write_packet:
+                        outfile.write("".join(buffer))
+                        buffer = []
 
+                    write_packet = False # reset for non icmp packets
+
+                elif write_packet:
+                    buffer.append(line)
+
+            # edge case for last being icmp
+            if buffer and write_packet:
+                outfile.write("".join(buffer))
 
         # Output
     except:
         print(f"Error: The file '{file_name}' does not exist.")
 
 
-filter()	
+filter('Node1.txt')	
