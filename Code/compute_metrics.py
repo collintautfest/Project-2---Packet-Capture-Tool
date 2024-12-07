60f1fb657f2f4b499097ec41cecc92073db05a92
collintautfest <ctautfest1@gmail.com>
2024-12-07 16:09:54 -0500

    compute metrics

--- Code/compute_metrics.py
+++ Code/compute_metrics.py
@@ -1,3 +1,22 @@
-def compute() :
-	print('called compute function in compute_metrics.py')
+#!/bin/python3
+
+import re
+
+# File path to your text file
+file_path = "Node1_filtered.txt"
+
+def compute(file_path):
+    # Counters
+    requests_sent = 0
+    requests_received = 0
+    replies_sent = 0
+    replies_received = 0
+    request_bytes_sent = 0
+    request_bytes_recieved = 0
+    request_data_sent = 0
+    request_data_recieved = 0
+
+
+
+compute(file_path)
 
